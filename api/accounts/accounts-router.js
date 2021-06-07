1b2db6c314edecfbecd86d7af211a3af96bf16b5
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-07 17:34:42 -0600

    Files connected and wrote getAll and getById helper function in accounts-model.js

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,16 +1,18 @@
-const router = require('express').Router()
+const express = require('express');
+const router = require('express').Router();
+const Account = require('./accounts-model');
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
@@ -18,10 +20,11 @@ router.put('/:id', (req, res, next) => {
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.use((err, req, res, next) => {
+  // eslint-disable-line
   // DO YOUR MAGIC
-})
+});
 
 module.exports = router;
